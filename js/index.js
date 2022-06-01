9b1c68e778a56c896cd26862d6d3ffe8abb8aa38
hassan <HassanMohsen1414@gmail.com>
2022-06-01 18:02:20 +0200

    added js logic

--- js/index.js
+++ js/index.js
@@ -2,13 +2,25 @@ var inputs = document.querySelectorAll('input')
 var answers = document.querySelectorAll('.answer')
 
 
+// function toggleElementDisplay(){
+// for (let i = 0; i < inputs.length; i++) {
+
+//     if(inputs[i].checked){
+//        answers[i].className = "d-block";
+//     }else{
+//         answers[i].className = "d-none";
+//     }
+    
+// }
+// }
 function toggleElementDisplay(){
 for (let i = 0; i < inputs.length; i++) {
 
     if(inputs[i].checked){
-       answers[i].className = "d-block";
-    }else{
-        answers[i].className = "d-none";
+       document.querySelector(`.${inputs[i].value}`).style.display = "block"
+       console.log(inputs[i].value);
+    }else if(inputs[i].checked == false){
+        document.querySelector(`.${inputs[i].value}`).style.display = ""
     }
     
 }
